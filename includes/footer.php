f5c96bb94fcdec04ba4e9dfea4dc427ba79cbdf4
Herliyanto Dwi Purnomo Syamsi <Herliyantodps@users.noreply.github.com>
2023-09-23 21:28:07 +0700

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,27 @@
+ <footer>
+     <div class="container adeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+         <div class="social text-center">
+             <a href="#"><i class="fa fa-twitter"></i></a>
+             <a href="#"><i class="fa fa-facebook"></i></a>
+             <a href="#"><i class="fa fa-instagram"></i></a>
+             <a href="#"><i class="fa fa-dribbble"></i></a>
+             <a href="#"><i class="fa fa-github"></i></a>
+         </div>
+
+         <div class="clear"></div>
+         <!--CLEAR FLOATS-->
+     </div>
+ </footer>
+ <!--/.page-section-->
+ <section class="copyright">
+     <div class="container">
+         <div class="row">
+             <div class="col-sm-12 text-center adeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+                 Copyright &copy;<script>
+                     document.write(new Date().getFullYear());
+                 </script> - Explore Dunia
+             </div>
+         </div>
+         <!-- / .row -->
+     </div>
+ </section>
